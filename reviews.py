105a5ee41f78dc1b42e92c7e22059019b3692905
Maggie  Little <maggielittle95@gmail.com>
2023-09-12 21:49:18 -0400

    updated with different version using merge function

--- reviews.py
+++ reviews.py
@@ -1,27 +1,17 @@
 import pandas as pd
-#wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
-#country_points = wine_reviews[['country', 'points']]
-#count_of_wines = (wine_reviews.country.value_counts())
-#print(country_points, count_of_wines,)
-
-import pandas as pd
 import zipfile
 
-# Specify the path to your .zip file
 zip_file_path = "data/winemag-data-130k-v2.csv.zip"
 
-# Extract the contents of the .zip file
 with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
     csv_file_name = zip_ref.namelist()[0]
     with zip_ref.open(csv_file_name) as csv_file:
         df = pd.read_csv(csv_file)
 
-country_counts = df['country'].value_counts()
-print(country_counts)
-
-points = df.groupby('country').points.mean().round(1)
-# print(points)
-answer = pd.merge(country_counts, points, on='country')
+wine_reviews = pd.read_csv("data/winemag-data-130k-v2.csv.zip")
+country_points = wine_reviews[['country', 'points']]
+count_of_wines = (wine_reviews.country.value_counts())
+#print(country_points, count_of_wines,)
+answer = pd.merge(country_points, count_of_wines, on='country')
 print(answer)
-
 answer.to_csv('data/reviews-per-country.csv')
